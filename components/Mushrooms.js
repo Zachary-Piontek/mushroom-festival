da43780086edcb59af2da3ea6d6eb775a2455c2f
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-06-15 12:37:02 -0700

    mushrooms displaying

--- components/Mushrooms.js
+++ components/Mushrooms.js
@@ -4,11 +4,15 @@ export default function createMushrooms(root) {
 
     return ({ mushrooms }) => {
         // *** clear out the container element
+        container.innerHTMl = '';
         // loop the mushrooms!
         // add one Mushroom component for each mushroom object
         //     - call the Mushroom component with the mushroom object
         //     - append the returned element to the container  
-
+        for (const mushroom of mushrooms) {
+            const image = Mushroom({ mushroom });
+            container.append(image);
+        }
     };
 }
 
